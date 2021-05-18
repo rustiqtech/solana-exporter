16e1ff2aa00cbdb37f242e3ecf639b3189baea7c
Anson Cheung <git@anson-cheung.com>
2021-05-18 21:09:19 +0800

    Add ip-api structs and functions

--- src/geolocation/api.rs
+++ src/geolocation/api.rs
@@ -1,2 +1,41 @@
-// TODO: Select an API provider for geolocation
-// TODO: Write functions for fetching from API
+use serde::{Serialize, Deserialize};
+use std::net::IpAddr;
+
+pub const IP_API: &str = "https://ip-api.com";
+
+/// Response from https://ip-api.com
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct IpApiResponse {
+    query: String,
+    status: String,
+    country: String,
+    country_code: String,
+    region: String,
+    region_name: String,
+    city: String,
+    zip: String,
+    lat: f64,
+    lon: f64,
+    timezone: String,
+    isp: String,
+    org: String,
+    #[serde(rename = "as")]
+    autonomous_system: String,
+}
+
+pub fn locate_ip(ip: &IpAddr) -> Result<IpApiResponse, reqwest::Error> {
+    reqwest::blocking::get(
+        format!("{}/json/{}", IP_API, ip.to_string())
+    )?.json::<IpApiResponse>()
+}
+
+pub fn locate_batch_ips(ips: &[IpAddr]) -> Result<Vec<IpApiResponse>, reqwest::Error> {
+    let client = reqwest::blocking::Client::new();
+    client.post(
+        format!("{}/batch", IP_API)
+    )
+        .json(ips)
+        .send()?
+        .json::<Vec<IpApiResponse>>()
+}
