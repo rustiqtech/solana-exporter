2d15a5cb083370124d8f0cd35dd3f8da5908cc60
Anson Cheung <git@anson-cheung.com>
2021-05-24 19:58:39 +0800

    Rename internal fields of MaxMindAPIKey

--- src/geolocation/api.rs
+++ src/geolocation/api.rs
@@ -1,20 +1,23 @@
 pub const MAXMIND_CITY_URI: &str = "https://geoip.maxmind.com/geoip/v2.1/city";
 
-pub struct MaxMindAPIKey(String, String);
+pub struct MaxMindAPIKey {
+    username: String,
+    password: String,
+}
 
 impl MaxMindAPIKey {
     pub fn new(username: &str, password: &str) -> Self {
         Self {
-            0: username.to_owned(),
-            1: password.to_owned(),
+            username: username.to_owned(),
+            password: password.to_owned(),
         }
     }
 
     pub fn username(&self) -> &str {
-        &self.0
+        &self.username
     }
 
     pub fn password(&self) -> &str {
-        &self.1
+        &self.password
     }
 }
