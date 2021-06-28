ac4f5d5373b88af2511c0b23540b04224039a99f
Anson Cheung <git@anson-cheung.com>
2021-06-29 04:45:34 +0800

    Add function for fetching rewards for a given epoch

--- src/epoch_credits/mod.rs
+++ src/epoch_credits/mod.rs
@@ -1,2 +1,36 @@
+use anyhow::anyhow;
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::clock::Epoch;
+use solana_sdk::epoch_info::EpochInfo;
+use solana_transaction_status::Rewards;
+
 pub mod caching;
 pub mod caching_metadata;
+
+const SLOT_OFFSET: u64 = 20;
+
+pub fn get_rewards_for_epoch(epoch: Epoch, client: &RpcClient) -> anyhow::Result<Rewards> {
+    let info = client.get_epoch_info()?;
+
+    // Convert epoch number to slot
+    let start_slot = epoch * info.slots_in_epoch;
+
+    // We cannot use an excessively large range if the epoch just started. There is a chance that
+    // the end slot has not been reached and strange behaviour will occur.
+    // If this is the current epoch and less than 10 slots have elapsed, then do not define an
+    // end_slot for use in the RPC call.
+    let end_slot = if info.epoch == epoch && info.slot_index < 10 {
+        None
+    } else {
+        Some(start_slot + SLOT_OFFSET)
+    };
+
+    // First block only
+    let block = client
+        .get_blocks(start_slot, end_slot)?
+        .get(0)
+        .cloned()
+        .ok_or(anyhow!("no blocks found"))?;
+
+    Ok(client.get_block(block)?.rewards)
+}
