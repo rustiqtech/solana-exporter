b9e048e2a707bd874f7e02b7ca133fd5100b6598
Anson <git@anson-cheung.com>
2021-08-15 23:13:12 +0800

    Fix cache being fully fetched

--- src/rewards/mod.rs
+++ src/rewards/mod.rs
@@ -1,5 +1,5 @@
 use crate::config::Whitelist;
-use crate::rewards::caching::RewardsCache;
+use crate::rewards::caching::{RewardsCache, PubkeyVoterApyMapping};
 use crate::rpc_extra::with_first_block;
 use anyhow::anyhow;
 use log::debug;
@@ -248,7 +248,11 @@ impl<'a> RewardsMonitor<'a> {
         });
 
         // Fetched pubkeys from cache
-        let cached_apys = self.cache.get_epoch_apy(current_epoch)?.unwrap_or_default();
+        let cached_apys = self.cache.get_epoch_apy(current_epoch)?
+            .unwrap_or_default()
+            .into_iter()
+            .filter(|(pk, _)| self.whitelist.contains(&pk.to_string()))
+            .collect::<PubkeyVoterApyMapping>();
 
         // Use cached pubkeys to find what keys we need to query
         let cached_pubkeys: BTreeSet<_> = cached_apys.keys().collect();
